19f2d3ef9948c5e4c2b5fdce80c29e6bda88bd6e
Emad Rashad <emadtab97@gmail.com>
2019-07-13 00:56:54 +0200

    Change Dummy name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,10 +61,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="/img/profile.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Emad Rashad</a>
         </div>
       </div>
-
+    
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
